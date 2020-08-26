f2019968963d8413aa630bafe6e50cc2bcd630dc
Crowdbotics <team@crowdbotics.com>
2020-08-26 18:31:04 +0000

    Update models

--- backend/tst_upgrade_dev_9395/wsgi.py
+++ backend/tst_upgrade_dev_9395/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_upgrade_dev_9395.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_upgrade_dev_9395.settings")
 
 application = get_wsgi_application()
